0f53c547eeb7c5195d40aa8a3c3bd6828263582f
sonprogrammer <ods04138@gmail.com>
2024-08-20 16:12:41 +0900

    Feat : login component

--- src/components/LoginComponent/LoginComponent.tsx
+++ src/components/LoginComponent/LoginComponent.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+/* eslint-disable */
+
+import React, { useState } from 'react'
 import KakaoLogin from 'react-kakao-login'
-import { StyledContainer, StyledEmailInput, StyledSubmitBtns } from './style'
+import { StyledBox, StyledContainer, StyledEmailInput, StyledLoginBtn, StyledLogo, StyledPasswordInput, StyledSubmitBtns } from './style'
 
 const LoginComponent = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    
+    
     const kakaoClientId = '748f4b889898873bb1a6d613886ebdf5'
+
+    const handleLogin = () =>{
+        console.log('try to login', { email, password})
+    }
+    
     const kakaoOnSuccess = async (data: any) => {
         console.log(data)
         const idToken = data.response.access_token  // 엑세스 토큰 백엔드로 전달
@@ -12,21 +23,40 @@ const LoginComponent = () => {
         console.log(error);
     };
     return (
-        <>
+        <StyledBox>
             <StyledContainer>
                 <h1>bnty login</h1>
-                <StyledEmailInput type="email" />
-                <StyledEmailInput type="password" />
+                <StyledLogo src="./logo2.png" alt="" />
+                <StyledEmailInput
+                    type="email" 
+                    placeholder='email'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    required
+                     />
+                <StyledPasswordInput 
+                    type="password" 
+                    placeholder='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                     />
                 <StyledSubmitBtns>
-                    <button type="submit">로그인하기</button>
+                    <StyledLoginBtn type="submit">로그인하기</StyledLoginBtn>
                     <KakaoLogin
                         token={kakaoClientId}
                         onSuccess={kakaoOnSuccess}
                         onFail={kakaoOnFailure}
+                        style={{
+                            backgroundColor: 'rgb(153 27 27)',
+                            color: 'white',
+                            borderRadius: '5px',
+                            padding: '10px 30px'
+                        }}
                     />
                 </StyledSubmitBtns>
             </StyledContainer>
-        </>
+        </StyledBox>
     )
 }
 
--- src/components/LoginComponent/style.ts
+++ src/components/LoginComponent/style.ts
@@ -1,32 +1,96 @@
 import tw from "twin.macro";
 import styled from 'styled-components'
 
+
+export const StyledBox = styled.div`
+    ${tw`
+        flex
+        h-screen
+        items-center
+        justify-center
+    `}
+`
+
 export const StyledContainer = styled.div`
     ${tw`
+        flex
+        flex-col
         border
-        border-red-950
+        border-red-700
         items-center
+        // justify-center
         text-center
+        w-[50%]
+        h-[70%]
+        // p-24
+        bg-neutral-600
     `}
     h1{
         ${tw`
-            text-3xl
+            text-[77px]
             text-red-900
+            mb-5
+            mt-3
         `}
+        font-family : "Anton"
     }
 `
+
+export const StyledLogo = styled.img`
+    ${tw`
+        w-[77px]
+        h-[77px]
+        mb-6
+    `}
+`
+
 
 export const StyledEmailInput = styled.input`
     ${tw`
         border
         border-black
-        flex
+        mb-4
+        w-[70%]
+        pl-[10px]
+        p-1
+        bg-stone-500
     `}
+    &:focus{
+        border : solid 2px;
+        border-color : rgb(127 29 29);
+        outline: none;
+    }
 `
+
+export const StyledPasswordInput = styled.input`
+    ${tw`
+        border
+        border-black
+        mb-4
+        w-[70%]
+        pl-[10px]
+        p-1
+        bg-stone-500
+    `}
+    &:focus{
+        border : solid 2px;
+        border-color : rgb(127 29 29);
+        outline: none;
+    }
+`
+
 export const StyledSubmitBtns = styled.div`
     ${tw`
         flex
         flex-col
+        mt-5
+    `}
+`
 
+export const StyledLoginBtn = styled.button`
+    ${tw`
+        mb-4
+        text-white
+        font-bold
     `}
 `
