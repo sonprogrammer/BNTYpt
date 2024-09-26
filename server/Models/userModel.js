95562979a8d91b9a076abaec4a920fb5c4f626c1
sonprogrammer <ods04138@gmail.com>
2024-09-26 17:00:22 +0900

    Feating : login

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     role:{
         type: String,
         enum: ['trainer', 'member'],
+        //* 멤버일 경우 남은 피티수업 횟수 보여주는거
         required: true
     },
 },{timestamps: true})
