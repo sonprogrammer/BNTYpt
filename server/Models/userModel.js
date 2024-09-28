efe6f89f2d3d485d7e65a3a2060e699e617216a3
sonprogrammer <ods04138@gmail.com>
2024-09-28 11:44:31 +0900

    Feating : login

--- server/Models/userModel.js
+++ server/Models/userModel.js
@@ -1,29 +1,31 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    kakaoId: {
-        type: String,
-        sparse: true,
-        unique: true
-    },
-    email: {
-        type: String,
-        unique: true
-    },
-    password: {
-        type: String,
-    },
-    name: {
-        type: String,
-    },
-    role:{
-        type: String,
-        enum: ['trainer', 'member'],
-        //* 멤버일 경우 남은 피티수업 횟수 보여주는거
-        required: true
-    },
-},{timestamps: true})
+  kakaoId: {
+    type: String,
+    sparse: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    unique: true
+  },
+  password: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  role: {
+    type: String,
+    enum: [
+      'trainer', 'member'
+    ],
+    //* 멤버일 경우 남은 피티수업 횟수 보여주는거
+    required: true
+  }
+}, {timestamps: true})
 
 const User = mongoose.model('User', userSchema)
- 
+
 module.exports = User
