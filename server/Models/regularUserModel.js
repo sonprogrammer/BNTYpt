02730986201c1e4882e90dda4dc4cafa87bede7a
sonprogrammer <ods04138@gmail.com>
2024-10-04 16:18:45 +0900

    Feat : login function

--- server/Models/regularUserModel.js
+++ server/Models/regularUserModel.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose');
 
 const regularUserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    role: {
-        type: String,
-        required: true,
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  }
 }, {timestamps: true})
 
 const regularUser = mongoose.model('regularUser', regularUserSchema)
