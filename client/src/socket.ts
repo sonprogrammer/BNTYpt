706fa5099840910e92ea50114103b03f38ff3778
sonprogrammer <ods04138@gmail.com>
2025-10-02 19:44:34 +0900

    Deploy

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,4 +1,4 @@
 import { io } from 'socket.io-client'
-const socket = io('http://localhost:4000')
+const socket = io(process.env.REACT_APP_SOCKET_URL)
 
 export default socket;
--- client/src/utils/socket.ts
+++
@@ -1,4 +0,0 @@
-// src/socket.ts
-import { io, Socket } from 'socket.io-client'
-
-export const socket: Socket = io(process.env.REACT_APP_API_URL)
